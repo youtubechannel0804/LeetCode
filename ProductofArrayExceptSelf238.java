abfc89469b2994beeafab65355d355b2b42928ec
youtubechannel0804 <47716570+youtubechannel0804@users.noreply.github.com>
2020-04-18 08:46:57 +0530

    Update ProductofArrayExceptSelf238.java

--- ProductofArrayExceptSelf238.java
+++ ProductofArrayExceptSelf238.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int product  = 1;
+        int zeroCount = 0;
+       for(int i=0;i<nums.length;i++){
+           if(nums[i] != 0){
+                product = product * nums[i];
+           }else{
+               zeroCount++;
+           }
+       }
+            for(int i=0;i<nums.length;i++){
+                if(nums[i] == 0){
+                    nums[i] = product  * (zeroCount > 1 ? 0:1);
+                }else{
+                   nums[i] = product/nums[i]  * (zeroCount>0 ? 0:1); 
+                }
+                
+            }
+        return nums;
+        
+    }
+}
